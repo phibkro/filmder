7ab7ced98f2d0f4a8808053c747e930faddca35a
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-12 16:38:28 +0200

    feat[Carousel]: add page functionality

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 import { MovieResult } from "../utils/types";
+
 import Card from "./Card";
+
 interface CarouselProps {
   results: MovieResult[];
 }
 export default function Carousel({ results }: CarouselProps) {
+  // Displays results using currentResult as index
+  const [currentResult, setCurrentResult] = useState(0);
+  function incrementCurrentResult() {
+    if (currentResult === results.length - 1) {
+      setCurrentResult(0);
+    } else {
+      setCurrentResult(currentResult + 1);
+    }
+  }
+  function decrementCurrentResult() {
+    if (currentResult === 0) {
+      setCurrentResult(results.length - 1);
+    } else {
+      setCurrentResult(currentResult - 1);
+    }
+  }
   return (
     <div className="carousel">
+      <p onClick={decrementCurrentResult}>Left</p>
       <Card imageSize="w1280" result={results[currentResult]} />
+      <p onClick={incrementCurrentResult}>Right</p>
     </div>
   );
 }
