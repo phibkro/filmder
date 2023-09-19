a87b8c06fc910edb1054d87043cb45ce66f5d736
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-19 15:10:06 +0200

    refactor(frontend): :recycle: update pages for new Card & Carousel structure

--- src/testing/pages/frontpage.tsx
+++ src/testing/pages/frontpage.tsx
@@ -6,14 +6,13 @@ import Card from "../../components/Card";
 import { getPopularMovies } from "../../utils/apiCalls";
 
 export default function FrontpageTest() {
-  const { isLoading, isError, data, error } = useQuery({
+  const { isLoading, isError, isSuccess, data, error } = useQuery({
     queryKey: ["popularMovies"],
     queryFn: getPopularMovies,
     refetchOnWindowFocus: false,
   });
   const [numberStore, addToStore, removeFromStore] =
     useNumberStore("favorites");
-  console.log(data);
   return (
     <>
       <main>
@@ -23,16 +22,40 @@ export default function FrontpageTest() {
         {/*@ts-ignore*/}
         {isError && <span>Error: {error.message}</span>}
 
-        {data && <Carousel results={data?.results} />}
+        {isSuccess && (
+          <Carousel
+            items={data?.results.map((result) => (
+              <Card
+                src={
+                  "https://image.tmdb.org/t/p/" +
+                  "original" +
+                  result.poster_path
+                }
+                alt={"Poster for the movie" + result.title}
+                href={"/movies/" + result.id}
+                size="large"
+                onAdd={() => addToStore(result.id)}
+                onRemove={() => removeFromStore(result.id)}
+                showStar={false}
+                key={result.id}
+              />
+            ))}
+          />
+        )}
 
         <ul className="filmList">
-          {data &&
+          {isSuccess &&
             data?.results.map((result) => (
               <li key={result.id}>
                 <Card
+                  src={
+                    "https://image.tmdb.org/t/p/" +
+                    "original" +
+                    result.poster_path
+                  }
+                  alt={"Poster for the movie" + result.title}
                   href={"/movies/" + result.id}
                   size="medium"
-                  result={result}
                   onAdd={() => addToStore(result.id)}
                   onRemove={() => removeFromStore(result.id)}
                   favorited={numberStore.includes(result.id)}
