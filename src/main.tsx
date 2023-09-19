ceebb77be01d1ade48b1fe034053a477977d450d
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-19 15:19:37 +0200

    fix(frontend): :ambulance: fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import { ThemeProvider } from "./features/theming/themeProvider.tsx";
 import "./styles/global.css";
-import Navbar from "./components/navbar.tsx";
-
 
 import { mainRoutes } from "./routing/mainRoutes.tsx";
 import { testRoutes } from "./testing/testRoutes.tsx";
@@ -17,7 +15,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Navbar></Navbar>
+      <ThemeProvider defaultTheme="dark" key="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
