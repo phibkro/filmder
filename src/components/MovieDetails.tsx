78591db0b9ad7767e70e55d56c3e341559134ccd
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-20 03:37:31 +0200

    refactor: :art: separate data fetching for pages & ui logic for components

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,27 +1,23 @@
 import { MovieDetails } from "../utils/types";
+import { createPosterUrl } from "../utils/lib";
+import Card from "./Card";
 
 interface MovieOverviewProps {
   movieDetails: MovieDetails;
 }
 export function MovieOverview({ movieDetails }: MovieOverviewProps) {
   return (
-    <div className="wrapper">
-      <div className="item1">
-        <img
-          src={
-            "https://image.tmdb.org/t/p/" +
-            "original" +
-            movieDetails.poster_path
-          }
-          alt={"Filmcover for" + movieDetails.original_title}
-        />
-      </div>
-      <div className="overviewWrapper">
+    <main className="movie-overview">
+      <Card
+        src={createPosterUrl(movieDetails.poster_path)}
+        alt={"Filmcover for" + movieDetails.original_title}
+        size="large"
+        showStar={false}
+      />
+      <section>
+        <h2>{movieDetails.original_title}</h2>
+        <p>{movieDetails.overview}</p>
         <div>
-          <h1>{movieDetails.original_title}</h1>
-          <p>{movieDetails.overview}</p>
-        </div>
-        <div className="spesificInfoWrapper">
           <div>
             <h3>Language</h3>
             <p>{movieDetails.original_language}</p>
@@ -55,7 +51,7 @@ export function MovieOverview({ movieDetails }: MovieOverviewProps) {
             <p>{movieDetails.popularity}</p>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
