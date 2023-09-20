ed1a271fdfdce1ab55a3f46c9be90e056a6848f6
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-20 17:36:45 +0200

    test(components): :test_tube: write up tests to do
    TODO: add test logic

--- src/test/components/Carousel.test.tsx
+++ src/test/components/Carousel.test.tsx
@@ -1,18 +1,24 @@
-import { describe, it, expect, afterEach } from "vitest";
 import { cleanup, render } from "@testing-library/react";
-
+import { afterEach, describe, expect, it } from "vitest";
 import Carousel from "../../components/Carousel";
+import data from "../mock/popularMovies.json";
 
 afterEach(cleanup);
+const items = data.results.map((result) => <li>{result.original_title}</li>);
 
-// TODO: test props and state
-describe("Carousel", () => {
-  it("should render", () => {
-    render(<Carousel items={} />);
+describe("Carousel props", () => {
+  it("should render with items", () => {
+    render(<Carousel items={items} />);
   });
+  it.todo("should render with left and right buttons");
+});
+describe("Carousel state", () => {
+  it.todo("should update its visible carousel item based on index state");
+  it.todo("should update index on click");
+  it.todo("index should be able to loop");
 });
 
 it("matches snapshot", async () => {
-  const result = render(<Carousel items={} />);
+  const result = render(<Carousel items={items} />);
   await expect(result).toMatchInlineSnapshot();
 });
