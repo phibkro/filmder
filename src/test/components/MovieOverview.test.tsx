c07de4ac06d22ac69d677cb6c623127b423a82b0
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-20 16:53:17 +0200

    test(components): :white_check_mark: add MovieOverview test

--- src/test/components/MovieOverview.test.tsx
+++ src/test/components/MovieOverview.test.tsx
@@ -1,14 +1,30 @@
-import { describe, it, expect, afterEach } from "vitest";
-import { cleanup, render } from "@testing-library/react";
-
+import { cleanup, render, screen } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import MovieOverview from "../../components/MovieOverview";
+import data from "../mock/barbie.json";
 
 afterEach(cleanup);
 
 // TODO: test props and state
 describe("MovieOverview", () => {
-  it("should render", () => {
-    render(<MovieOverview movieDetails={} />);
+  beforeEach(() => {
+    render(<MovieOverview movieDetails={data} />);
+  });
+  it("should render with props", async () => {
+    await screen.findByRole("main");
+
+    expect(screen.findByRole("img")).toBeTruthy();
+    expect(screen.findByAltText("Filmcover for" + data.original_title));
+
+    expect(screen.findByDisplayValue(data.original_title)).toBeTruthy();
+    expect(screen.findByDisplayValue(data.overview)).toBeTruthy();
+    expect(screen.findByDisplayValue(data.original_language)).toBeTruthy();
+    data.genres.map((genre) =>
+      expect(screen.findByDisplayValue(genre.name)).toBeTruthy(),
+    );
+    expect(screen.findByDisplayValue(data.release_date)).toBeTruthy();
+    expect(screen.findByDisplayValue(data.vote_average)).toBeTruthy();
+    expect(screen.findByDisplayValue(data.popularity)).toBeTruthy();
   });
 });
 
