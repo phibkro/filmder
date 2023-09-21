9111aef70ba17e4ab0b97737602bd2c975e3d48b
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-09-21 16:06:37 +0200

    style: :lipstick: restrict button styles to theme-toggle-button

--- src/components/ui/ThemeToggleButton.tsx
+++ src/components/ui/ThemeToggleButton.tsx
@@ -10,12 +10,18 @@ export default function ThemeToggleButton({ iconSize = "24px" }: Props) {
     <>
       {/* TODO: make button find the opposite of system theme */}
       {theme === "dark" && (
-        <button onClick={() => setTheme("light")}>
+        <button
+          className="theme-toggle-button"
+          onClick={() => setTheme("light")}
+        >
           <Sun size={iconSize} />
         </button>
       )}
       {theme === "light" && (
-        <button onClick={() => setTheme("dark")}>
+        <button
+          className="theme-toggle-button"
+          onClick={() => setTheme("dark")}
+        >
           <Moon size={iconSize} color="black" />
         </button>
       )}
