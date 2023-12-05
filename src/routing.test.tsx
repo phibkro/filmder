1b6b69bd96e1067d9e32106c4494d1c7430aec70
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-12-05 22:28:48 +0100

    revert: :fire: remove vite

--- src/routing.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { RouterProvider } from "react-router-dom";
-import { expect, it } from "vitest";
-import { appRouter } from "./router";
-
-it("matches snapshot", () => {
-  const result = render(<RouterProvider router={appRouter} />);
-  expect(result).toMatchInlineSnapshot();
-});
--- src/server/api.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe } from "vitest";
-
-// TODO
-
-describe.todo("usePopularMovies");
-
-describe.todo("useMovieById");
-
-// see https://tanstack.com/query/latest/docs/react/guides/testing
