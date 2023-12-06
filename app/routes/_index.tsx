558d14b02248f5d59c5970a5c8e6db44ef0d0757
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2023-12-06 01:27:35 +0100

    feat: :sparkles: add hydration in main page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,38 @@
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/router";
-import { useQuery } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  useQuery,
+} from "@tanstack/react-query";
 import { getPopularMovies } from "~/api";
 import MovieApp from "~/components/MovieApp";
 
 export async function loader() {
-  const popularMovies = await getPopularMovies();
-  return json({ popularMovies });
-}
-export default function Index() {
-  const { popularMovies } = useLoaderData<typeof loader>();
-
-  const { isLoading, isError, isSuccess, data, error } = useQuery({
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
     queryKey: ["popularMovies"],
     queryFn: getPopularMovies,
-    initialData: popularMovies,
+  });
+  return json({ dehydratedState: dehydrate(queryClient) });
+}
+
+function Movies() {
+  const { data } = useQuery({
+    queryKey: ["popularMovies"],
+    queryFn: getPopularMovies,
   });
 
+  return <MovieApp movieListResults={data.results} />;
+}
+
+export default function Index() {
+  const { dehydratedState } = useLoaderData<typeof loader>();
+
   return (
-    <>
-      {isError && <span>{error.message}</span>}
-      {isLoading && <span>...loading</span>}
-      {isSuccess && <MovieApp movieListResults={data.results} />}
-    </>
+    <HydrationBoundary state={dehydratedState}>
+      <Movies />
+    </HydrationBoundary>
   );
 }
